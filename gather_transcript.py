82037db06caf2e3dff6d0863303ff686c02d88bb
Tyler <tylerbouma1@gmail.com>
2021-06-04 12:55:12 -0500

    hid API key

--- gather_transcript.py
+++ gather_transcript.py
@@ -1,7 +1,8 @@
 from apiclient.discovery import build
 from youtube_transcript_api import YouTubeTranscriptApi
+import config
  
-api_key = "AIzaSyCcaq5p47xgD_YoRhGntQ8ASfUo8Tvo0wM"  # replace it with your API key
+api_key = config.api_key  # replace it with your API key
 channel_id = 'UCwmZiChSryoWQCZMIQezgTg'  # replace it with your channel id
 youtube = build('youtube', 'v3', developerKey=api_key)
  
@@ -38,8 +39,12 @@ for video_id in video_ids:
     try:
         responses = YouTubeTranscriptApi.get_transcript(
             video_id, languages=['en'])
-        print('\n'+"Video: "+"https://www.youtube.com/watch?v="+str(video_id)+'\n'+'\n'+"Captions:")
-        file_obj = open("Test"+str(counter)+".txt", "a")
+        vid = youtube.videos().list(id=video_id, part='snippet').execute()
+        vid_title = vid['items'][0]['snippet']['title']    
+        vid_link = "Video: "+"https://www.youtube.com/watch?v="+str(video_id)+'\n'+'\n'
+        file_obj = open(r"BBCE_transcripts/" + str(video_id) + str(counter)+".txt", "a")
+        file_obj.write("Title: " + vid_title + str("\n"))
+        file_obj.write(vid_link + str("\n"))
         for response in responses:
             text = response['text']
             # Write transcripts out for each video to a file

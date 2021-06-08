71b60c00b84a4c9192fffb978452f18c1ce7d90c
Tyler <tylerbouma1@gmail.com>
2021-06-08 09:43:20 -0500

    adjusted api endpoints to query db for title or vid link

--- api.py
+++ api.py
@@ -1,61 +1,48 @@
 from flask import Flask, request, jsonify
-import csv
+import pymongo
+from bson.json_util import dumps
 from __main__ import app
+
 
 app.config["DEBUG"] = True
 
+client = pymongo.MongoClient('localhost', 27017)
+db = client["BBCE"]
+col = db["Transcripts"]
+
 # A route to return all of the available entries
-@app.route('/api/v1/resources/sites/all', methods=['GET'])
+@app.route('/api/v1/resources/transcripts/all', methods=['GET'])
 def api_all():
-    with open('datasets/MAPPPD_data.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        big_list = []
-        for row in csv_reader:
-            big_list.append(row)
-
-
-    return jsonify(big_list)
+    cur = col.find()
+    list_cur = list(cur)
+    return dumps(list_cur)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-@app.route('/api/v1/resources/sites', methods=['GET'])
+@app.route('/api/v1/resources/transcripts', methods=['GET'])
 def api_filter():
     # Get the query request from the URL
     query_params = request.args
 
-    # All column items
-    # site name,site id,ccamlr region,Longitude EPSG:4326,Latitude EPSG:4326,common name,day,month,year,season starting,count,accuracy,count type,vantage,reference,notes
-
     # Items that can be used to filter
-    site_name = query_params.get('site_name')
-    site_id = query_params.get('site_id')
-    common_name = query_params.get('common_name')
-    year = query_params.get('year')
+    title = query_params.get('title')
+    vid_link = query_params.get('vidlink')
 
-    to_filter = []
+    to_filter = {}
     big_list = []
 
-    if site_name:
-        to_filter.append(site_name)
-    if site_id:
-        to_filter.append(site_id)
-    if common_name:
-        to_filter.append(common_name)
-    if year:
-        to_filter.append(year)
-    if not (id or published or author):
+    if title:
+        to_filter = { "title": title }
+    if vid_link:
+        to_filter = { "vidlink": vid_link }
+    if not (title or vid_link):
         return page_not_found(404)
 
-    # check csv for all of our filter items
-    with open('datasets/MAPPPD_data.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        for row in csv_reader:
-            if set(to_filter).issubset(row):
-                big_list.append(row)
-    
-    
-    return jsonify(big_list)
+    # check database for item
+    cur = col.find(to_filter)
+    list_cur = list(cur)
+    return dumps(list_cur)
     
 app.run()

d8b5f191ba050c83ff958dcd4d90ef3e058594c0
Tyler <tylerbouma1@gmail.com>
2021-06-08 16:16:39 -0500

    Changed about page to return the README file contents

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify, render_template
+import markdown
+import markdown.extensions.fenced_code
 
 app = Flask(__name__)
 
@@ -6,5 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# import declared routes - needs to be after the 
+@app.route('/about')
+def about():
+    # display the contents of the README
+    readme_file = open("README.md", "r")
+    md_template_string = markdown.markdown(
+        readme_file.read(), extensions=["fenced_code"]
+    )
+
+    return render_template('about.html') + md_template_string
+
+# import declared routes - needs to be after the app page
 import api
+
+if __name__ == "__main__":
+    app.run()
